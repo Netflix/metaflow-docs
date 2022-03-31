80886a93182c7db22f0e753f58811ca5d2cb189c
Brendan Gibson <brendan@outerbounds.co>
2022-03-31 07:23:58 -0600

    Turn off contextual search and search page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,7 +87,7 @@ const config = {
         indexName: "metataflow",
 
         // Optional: see doc section below
-        contextualSearch: true,
+        contextualSearch: false,
 
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         externalUrlRegex: "external\\.com|domain\\.com",
@@ -96,7 +96,7 @@ const config = {
         searchParameters: {},
 
         // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: "search",
+        searchPagePath: false,
 
         //... other Algolia params
       },
